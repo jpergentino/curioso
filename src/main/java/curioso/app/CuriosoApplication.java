56afa1048509380f928072683d08749b70861ddd
pergentino <jpergentino@gmail.com>
2025-04-22 17:04:11 +0200

    First commit.

---
+++ src/main/java/curioso/app/CuriosoApplication.java
@@ -0,0 +1,13 @@
+package curioso.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CuriosoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CuriosoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/curioso/app/CuriosoApplicationTests.java
@@ -0,0 +1,13 @@
+package curioso.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CuriosoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
