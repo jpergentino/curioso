5dcdf76c53b759da0b9c4d67c32d872bbabc7db7
pergentino <jpergentino@gmail.com>
2025-04-22 17:11:02 +0200

    Adding a Web config and reorganizing the project’s structure.

--- src/main/java/curioso/CuriosoApplication.java
+++ src/main/java/curioso/CuriosoApplication.java
@@ -1,4 +1,4 @@
-package curioso.app;
+package curioso;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ src/main/java/curioso/conf/WebConfig.java
@@ -0,0 +1,25 @@
+package curioso.conf;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * Web Configuration. Mainly to expose the UI Angular application built on $project/src/main/resources/static folder.  
+ */
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/{path:[^\\.]*}")
+                .setViewName("forward:/index.html");
+    }
+    
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**")
+                .addResourceLocations("classpath:/static/");
+    }
+}
